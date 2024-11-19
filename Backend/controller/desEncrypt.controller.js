ebb4deaf3d2cbb485a561ea764866de41cac1414
kishoear <kishorear.programming@gmail.com>
2024-11-19 23:33:54 +0530

    Backend updated

--- Backend/controller/desEncrypt.controller.js
+++ Backend/controller/desEncrypt.controller.js
@@ -1,6 +1,7 @@
 const desEncrypt = (req, res) => {
     const text = req.body.text;
     console.log(text);
+    res.status(200).json({ status : "ok" });
 }
 
 export default desEncrypt;
--- Backend/index.js
+++ Backend/index.js
@@ -3,17 +3,22 @@ import aes from './routes/aes.route.js'
 import des from './routes/des.route.js'
 import rsa from './routes/rsa.route.js'
 import rc4 from './routes/rc4.route.js'
+import cors from 'cors';
 
 const app = express();
 
-
-app.listen(3000, () =>
-{
-    console.log("Server running!");
-})
+app.use(cors({
+    origin: 'http://localhost:5173', // Allow your React app's origin
+    methods: ['GET', 'POST'],        // Allowed methods
+    credentials: true                // If you're using cookies/auth headers
+}));
 
 app.use(express.json());
 app.use('/aes', aes);
 app.use('/des', des);
 app.use('/rsa', rsa);
 app.use('/rc4', rc4);
+
+app.listen(3000, () => {
+    console.log("Server running!");
+})
